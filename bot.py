637003c44222daef21ffcfec20133358e14cd098
rivo323 <pypury@gmail.com>
2026-09-20 14:34:20 +0300

    Make bot respond to start

--- bot.py
+++ bot.py
@@ -1,10 +1,33 @@
 import os
 import requests
+import time
 
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
-url = f"https://api.telegram.org/bot{TOKEN}/getMe"
+offset = 0
 
-response = requests.get(url)
+while True:
+    response = requests.get(
+        f"https://api.telegram.org/bot{TOKEN}/getUpdates",
+        params={"offset": offset, "timeout": 20}
+    )
 
-print(response.json())
+    data = response.json()
+
+    for update in data.get("result", []):
+        offset = update["update_id"] + 1
+
+        message = update.get("message")
+
+        if message and message.get("text") == "/start":
+            chat_id = message["chat"]["id"]
+
+            requests.post(
+                f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+                data={
+                    "chat_id": chat_id,
+                    "text": "היי ❤️ הבוט עובד!"
+                }
+            )
+
+    time.sleep(1)
